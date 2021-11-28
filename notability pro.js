68112e959627157c4dc819759b80dc19da7fc61e
zwdzzs1 <62322668+zwdzzs1@users.noreply.github.com>
2021-11-28 08:53:20 +0800

    Update notability pro.js

--- notability pro.js
+++ notability pro.js
@@ -16,7 +16,7 @@
 [rewrite_local]
 
 # Notability解锁订阅
-^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/NotabilityProCrack.js
+^https?:\/\/notability\.com\/subscriptions url script-response-body https://github.com/zwdzzs1/hello-world/blob/main/notability%20pro.js
 
 [mitm] 
 

25419c097b3bea1d81470d293e29d3ed91d27006
zwdzzs1 <62322668+zwdzzs1@users.noreply.github.com>
2021-11-28 09:02:47 +0800

    Update photoshop.js

--- photoshop.js
+++ photoshop.js
@@ -1,6 +1,6 @@
 /*
 解锁 Photoshop for iPad
-
+[rewrite_local]
 ^https:\/\/lcs-mobile-cops\.adobe\.io\/mobile_profile\/nul\/v1$ url script-response-body photoshop.js
 [mitm]
 Mitm = lcs-mobile-cops.adobe.io
